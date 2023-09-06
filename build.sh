59e4f33038aed828d390709029fee46a1131627f
MariaEsquivelf <131416497+MariaEsquivelf@users.noreply.github.com>
2023-09-06 18:35:42 -0400

    updates

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+# poetry install
+pip install -r requirements.txt
 
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py migrate
